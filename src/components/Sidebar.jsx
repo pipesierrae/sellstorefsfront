3a09f3425949dac19de34892585b4a132a9357aa
felipesierrae <felipesierrae@mail.com>
2021-10-29 12:01:31 -0500

    se agrega darmode, tablas y nuevos estilos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import TriggerDarkMode from './TriggerDarkMode';
 
 const Sidebar = () => {
-  return <nav className='w-72 bg-red-400 h-full flex flex-col justify-between'>Sidebar</nav>;
+  return (
+    <nav className='w-72 border border-gray-300 h-full flex flex-col justify-between'>
+      <TriggerDarkMode />
+    </nav>
+  );
 };
 
 export default Sidebar;
